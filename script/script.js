5fbd6bd2a89c3d749c60968cae05bc71f99668b8
Knotmaxx <diesknot@gmail.com>
2023-05-12 10:49:35 -0300

    commit all

--- script/script.js
+++ script/script.js
@@ -1,13 +1,37 @@
 const ul = document.querySelectorAll("ul")
 const navbar = document.querySelector(".collapse")
 const btnNav = document.querySelector(".navbar-toggler")
-
-ul.forEach(li => {
-  li.addEventListener("click",FecharMenu)
-})
+const img_about = document.querySelector(".about-img")
 
 function FecharMenu() {
   if (getComputedStyle(btnNav).display == "block") {
     btnNav.click()
   }
 }
+
+window.addEventListener("load" , function(){
+  if(window.innerWidth <= 767){
+    img_about.src = "imgs/fundo/fundo about/japanese-mobile.jpg"
+    
+  }else{
+    img_about.src = "imgs/fundo/fundo about/japanese-3723045_1920.jpg"
+  }
+})
+
+window.addEventListener("resize", function() {
+  if(window.innerWidth <= 767){
+    img_about.src = "imgs/fundo/fundo about/japanese-mobile.jpg"
+    
+  }else{
+    img_about.src = "imgs/fundo/fundo about/japanese-3723045_1920.jpg"
+  }
+})
+
+
+
+ul.forEach(li => {
+  li.addEventListener("click",FecharMenu)
+})
+
+
+
